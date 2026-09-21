229cd5d70487569ef730ba6a223a81684bd01977
Aster1994 <mina.shafiei18@gmail.com>
2026-09-21 23:42:30 +0330

    refactor: set guards and handle states for better UI

--- app/pages/products/[id].vue
+++ app/pages/products/[id].vue
@@ -1,11 +1,20 @@
 <template>
-    <div class="mx-auto max-w-max-width-container py-6">
+    <BaseCard v-if="status === 'pending'" class="grid grid-cols-1 sm:grid-cols-[172px_1fr] my-6 items-center gap-3">
+        <BaseSkeleton class="col-span-2 w-full h-[310px]" rounded="lg"/>
+        <div v-for="i in 4" :key="i"
+             class="flex flex-col gap-3">
+            <BaseSkeleton class="h-12"/>
+            <BaseSkeleton class="h-12"/>
+        </div>
+    </BaseCard>
+
+    <div v-else-if="status === 'success' && product" class="mx-auto max-w-max-width-container py-6">
         <Breadcrumb
             :items="[
-        { label: categoryLabel, to: `/products?category=${product.category}` },
+        { label: categoryLabel || 'لیست محصولات', to: `/products?category=${product.category}` },
         { label: product.title },
       ]"
-            class="py-6"
+            class="pb-6"
         />
 
         <div class="flex flex-col gap-6">
@@ -32,11 +41,11 @@
                 <div v-for="(row, i) in tableData" :key="i"
                      class="grid grid-cols-1 sm:grid-cols-[172px_1fr] items-center gap-3">
                     <div
-                        class="p-[13px] h-full text-sm font-medium text-gray-600 bg-gray-25 rounded-l-sm rounded-r-2xl">
+                        class="p-[13px] min-h-12 h-full text-sm font-medium text-gray-600 bg-gray-25 rounded-2xl sm:rounded-l-sm sm:rounded-r-2xl">
                         {{ row.title }}
                     </div>
                     <div
-                        class="p-[13px] h-full text-sm font-bold text-gray-800 bg-gray-25 rounded-r-sm rounded-l-2xl">
+                        class="p-[13px] min-h-12 h-full text-sm font-bold text-gray-800 bg-gray-25 rounded-2xl sm:rounded-r-sm sm:rounded-l-2xl">
                         {{ row.value }}
                     </div>
 
@@ -45,6 +54,10 @@
 
         </div>
     </div>
+
+    <EmptyState v-else-if="status === 'success' && !product" class="my-5"/>
+
+    <ErrorSate v-else-if="status === 'error'" @retry="refresh"/>
 </template>
 
 <script lang="ts" setup>
@@ -53,11 +66,17 @@ import {getCategoryLabel} from '~/constants/product'
 import Breadcrumb from "~/components/layout/Breadcrumb.vue";
 import BaseCard from "~/components/ui/BaseCard.vue";
 import {commaSeparator} from "~/utils/formatNumer.ts";
+import EmptyState from "~/components/ui/EmptyState.vue";
+import ErrorSate from "~/components/ui/ErrorSate.vue";
+import BaseSkeleton from "~/components/ui/BaseSkeleton.vue";
 
 const route = useRoute()
-const {data: product, error} = await useProduct(() => route.params.id as string)
-
-const categoryLabel = computed(() => getCategoryLabel(product.value!.category))
+const {data: product, status, error, refresh} =
+    await useProduct(() => route.params.id as string)
+const categoryLabel = computed(() => {
+    if (!product.value) return ""
+    return getCategoryLabel(product.value!.category)
+})
 
 
 const tableData = [
@@ -84,16 +103,12 @@ const tableData = [
 
 ]
 
-if (!product.value) {
-    throw createError({statusCode: 404, statusMessage: 'محصول یافت نشد', fatal: true})
-}
-
 useSeoMeta({
-    title: product.value.title,
-    description: product.value.description,
-    ogTitle: product.value.title,
-    ogDescription: product.value.description,
-    ogImage: product.value.image,
+    title: product.value?.title,
+    description: product.value?.description,
+    ogTitle: product.value?.title,
+    ogDescription: product.value?.description,
+    ogImage: product.value?.image,
 })
 
 </script>
